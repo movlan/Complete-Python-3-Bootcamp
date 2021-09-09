8a129f1a1ee6aca7b9f8c6b2a71a69d4e0dc27a4
Bahram Movlanov <movlan@users.noreply.github.com>
2021-09-09 17:07:21 -0600

    End of Section 7

---
+++ my_code/myexample.py
@@ -0,0 +1,34 @@
+a = {1,2,3}
+b = {3,2,1}
+
+print(a == b)
+
+
+# x = 50
+
+# def func(): 
+#   x = 2
+#   return 10 * x
+
+# print(x)
+
+# x = func()
+
+# print(x)
+
+# def myfunc(s):
+#     new_s = ''
+    
+#     for n in range(len(s)):
+#       print(n)
+#       if n % 2 == 0:
+#           new_s = new_s + s[n].lower()
+#       else:
+#           new_s = new_s + s[n].upper()
+  
+#     return new_s
+
+# def myfunc(name):
+#     print('Hello {name}'.format(name=name))
+
+# print(myfunc('Name'))
